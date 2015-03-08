be3bea7587193ed29d370829b632f8f8b4ad10e1
icaustonltd <ian.causton@icaustonltd.com>
2015-03-08 09:02:41 +0000

    Picture fill updates

--- assets/js/slipstream.js
+++ assets/js/slipstream.js
@@ -1,25 +1,19 @@
-(function($) {
-   $( 'a[href="#"]' ).click( function(e) {
-      e.preventDefault();
-   } );
-} )(jQuery);
-
-
-$(".more-info").click(function(){
-
-	// reset
-	$(".post-content").hide();
-	$(".promo-image-button").show();
-
-	$(this).parent().siblings(".post-content").show();
-    $(this).parent().hide();
+$(document).ready(function() {
+ 
+  $("#slipstream-carousel").owlCarousel({
+      autoPlay : 3000,
+      stopOnHover : true,
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem: true,
+      transitionStyle: "fade"
+    });
+ 
 });
 
-$.fn.carousel.defaults = {
-    interval: false
-  , pause: 'hover'
-}
-
+// Randonly swap photos of team on the homepage
 var images = ['team_member_yuri.gif', 'team_member_andy.gif', 'team_member_ian.gif'];
 
 $('<img src="/assets/img/the_team/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#team-promo');
+
